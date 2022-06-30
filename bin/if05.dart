d4fac28c6ad65a9735fbf0eac607d190cc94976b
saloh7856 <salohiddinberdiyorov2002@gmail.com>
2022-06-30 08:04:56 +0500

    && 3 ta son berilgan ularni manfiylari sonoini topish

--- bin/if05.dart
+++ bin/if05.dart
@@ -8,4 +8,37 @@
         c: integer
     returns:
         integer: the number of negative numbers in the given numbers*/
-void main() {}
+int func(int a,b,c){
+ 
+     if(a>0 && b>0 && c<0){
+         return 1;
+     }
+     if(a<0 && b>0 && c>0){
+         return 1;
+     }
+      if(a>0 && b<0 && c>0){
+         return 1;
+     }
+     if(a>0 && b<0 && c<0){
+         return 2;
+     }
+     if(b>0 && a<0 && c<0){
+         return 2;
+     }
+     if(c>0 && b<0 && a<0){
+         return 2;
+     }
+     if(a>0 && b>0 && c>0){
+        return 0;
+     }
+    if(a<0 && b<0 && c<0){
+         return 3;
+     }
+    
+ }
+void main() {
+    print(func(2,5,22));
+}
+
+
+
