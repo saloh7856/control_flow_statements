726d88580aaa4a0b18509cc47b58016093392dd9
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-06-30 16:43:27 +0100

    number uzgaruvchisi pasitive bulsa 1 ga ortsin negative bulsa 2 ga kamaysin

--- bin/if02.dart
+++ bin/if02.dart
@@ -1,20 +1,21 @@
 /*
     */
-Create function called func
-    Create a function argument  called 'number' of type int
-    If the 'number' is positive, increase it to 1, else decrease it to 2.
-    Args:
-        a: integer
-    Returns:
-        a: a increased by 1 if positive, else decreased by 2.
+// Create function called func
+//     Create a function argument  called 'number' of type int
+//     If the 'number' is positive, increase it to 1, else decrease it to 2.
+//     Args:
+//         a: integer
+//     Returns:
+//         a: a increased by 1 if positive, else decreased by 2.
 int func(int number){
     if(number>0){
-        return (number+1);
+        return number+1;
     }
     if(number<0){
-        return (number-2);
+        return number-2;
     }
     }
 void main() {
-    return (func(-1));
+
+    print(func(-1));
 }
