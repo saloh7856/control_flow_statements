1207b3a2bdbaa17ae1f2fd578c1bc910e449a086
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-01 17:50:53 +0500

    dan

--- bin/if07.dart
+++ bin/if07.dart
@@ -12,27 +12,30 @@
     Returns:
         string: the message to print */
 String func(int number){
-    if(number%2>0){
-        return ('positive odd number');
-       
-    }if(number>0){
-    if(number%2==0){
-        return ('positive even number');
-        
-    }}if(number%2<0){
-        return ('negative odd number');
-       
-    }if(number<0){
-    if(number%2==0){
-        return ('negative even number');
-      
-    }}if(number==0){
+
+    if(number>0){
+        if(number%2==0){
+            return ('positive even number');  
+        }
+        if(number%2==1){
+            return ('positive odd number');  
+        }
+    }
+    
+    if(number<0){
+        if(number%2==0){
+            return ('negative even number');
+        }
+        if(number%2==1){
+            return ('negative odd number');
+        }
+    }
+    if(number==0){
         return ('the number is zero');
-        
     }
 }
 
 void main(){
-    print(func(-10));
+    print(func(120));
 }
 
