64f7aab27115cd0f300997d4a2ebd538954a321a
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-01 09:08:57 +0500

    uzgaruvchi juft,toq,manfiy juft,manfiy toq, noll ligini aniqlash

--- bin/if07.dart
+++ bin/if07.dart
@@ -11,4 +11,28 @@
         a: integer
     Returns:
         string: the message to print */
-void main() {}
+String func(int number){
+    if(number%2>0){
+        return ('positive odd number');
+       
+    }if(number>0){
+    if(number%2==0){
+        return ('positive even number');
+        
+    }}if(number%2<0){
+        return ('negative odd number');
+       
+    }if(number<0){
+    if(number%2==0){
+        return ('negative even number');
+      
+    }}if(number==0){
+        return ('the number is zero');
+        
+    }
+}
+
+void main(){
+    print(func(-10));
+}
+
